abe968d8c9c908e5599a34ce457620cc85b6c389
melihogul <ogul1905@gmail.com>
2023-09-10 23:45:16 +0300

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,46 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import '../globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import Topbar from '@/components/shared/Topbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSideBar from '@/components/shared/RightSideBar'
+import Bottombar from '@/components/shared/Bottombar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Threads',
+  description: 'Meta Threads Application'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <head><link rel="icon" href="/images/favicon.ico" /></head>
+        <body className={inter.className}>
+          <Topbar/>
+
+          <main className='flex flex-row'>
+            <LeftSidebar/>
+
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>
+                {children}
+              </div>
+            </section>
+
+            <RightSideBar/>
+          </main>
+
+          <Bottombar/>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
