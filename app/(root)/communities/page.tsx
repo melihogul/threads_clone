f690f677efe1ff7ff126b4204f40049eb409de7e
melihogul <ogul1905@gmail.com>
2023-09-11 01:12:52 +0300

    implement features

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,11 +1,54 @@
-const Page = async() => {
-    return (
-      <section>
-          <h1 className="head-text mb-10">
-              Communities
-          </h1>
-      </section>
-    )
-  }
-  
-  export default Page
+import ProfileHeader from "@/components/shared/ProfileHeader"
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions"
+import { currentUser } from "@clerk/nextjs"
+import { profileTabs } from "@/constants"
+import Image from "next/image"
+import ThreadsTab from "@/components/shared/ThreadsTab"
+import { redirect } from "next/navigation"
+import UserCard from "@/components/cards/UserCard"
+import { fetchCommunities } from "@/lib/actions/community.actions"
+import CommunityCard from "@/components/cards/CommunityCard"
+
+async function Page () {
+    const user = await currentUser()
+    if(!user) return null
+
+    const userInfo = await fetchUser(user.id)
+    if(!userInfo?.onboarded) redirect('/onboarding')
+
+    const result = await fetchCommunities({
+        searchString: '',
+        pageNumber: 1,
+        pageSize: 25
+    })
+    
+  return (
+    <section>
+        <h1 className="head-text mb-10">
+            Communities
+        </h1>
+
+        <div className="mt-14 flex flex-col gap-9">
+            {result.communities.length === 0 ? (
+                <p className="no-result">No Communities</p>
+            ) : (
+                <>
+                    {result.communities.map((community) => (
+                        <CommunityCard
+                            key={community.id}
+                            id={community.id}
+                            name={community.name}
+                            username={community.username}
+                            imgUrl={community.image}
+                            bio={community.bio}
+                            members={community.members}
+                        />
+                    ))}
+                </>
+            )}
+        </div>
+    </section>
+  )
+}
+
+export default Page
